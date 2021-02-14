9a617ef1d93e08ff586614baae53375541d651a9
Rigel Iksandy Marthana <seimasu88@gmail.com>
2021-02-14 21:18:04 +0700

    home.dart
    home.dart oke

---
+++ lib/enter.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'class_penangkap.dart';
+
+class EntryForm extends StatefulWidget {
+  final ClassPenangkap contact;
+
+  EntryForm(this.contact);
+
+  @override
+  _EntryFormState createState() => _EntryFormState(this.contact);
+}
+
+class _EntryFormState extends State<EntryForm> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
